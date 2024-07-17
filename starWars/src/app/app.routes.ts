e4e9022f2d15e053dfd4c32aefa9f7aabde7cc14
claudiaamprimo <claudia.amprimo@gmail.com>
2024-07-17 18:59:41 +0200

    ejercicio 6 avance estilos forms

--- starWars/src/app/app.routes.ts
+++ starWars/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StarshipListComponent } from './starship-list/starship-list.component';
 import { StarshipDetailsComponent } from './starship-details/starship-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'starships', component: StarshipListComponent },
-  { path: 'starships/:id', component: StarshipDetailsComponent }
+  { path: 'starships/:id', component: StarshipDetailsComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
 ];
