7537e7a96875de2c2c7755bb6393e40901576123
claudiaamprimo <claudia.amprimo@gmail.com>
2024-07-21 17:53:09 +0200

    ejercicio 7 guards

--- starWars/src/app/app.routes.ts
+++ starWars/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { StarshipListComponent } from './starship-list/starship-list.component';
 import { StarshipDetailsComponent } from './starship-details/starship-details.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'starships', component: StarshipListComponent },
-  { path: 'starships/:id', component: StarshipDetailsComponent },
+  { path: 'starships', component: StarshipListComponent, canActivate: [AuthGuard] },
+  { path: 'starships/:id', component: StarshipDetailsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
 ];
